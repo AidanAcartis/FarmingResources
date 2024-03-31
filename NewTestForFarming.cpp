01583b62470785d9901244897f95100f9ae3c99b
AidanAcartis <ralaikotonadia@gmail.com>
2024-03-31 20:12:50 +0300

    fonction ForPossiblePath

--- NewTestForFarming.cpp
+++ NewTestForFarming.cpp
@@ -13,7 +13,7 @@ int CountPath(vector<int> Test, vector<int> loggingCamp, vector<int> friendlyTro
 vector<vector<int>> forMove(vector<int> Test, vector<vector<int>> impassableCells);
 int Verif(vector<int> Test, vector<vector<int>> impassableCells);
 vector<vector<int>> forChange(vector<int> Move);
-vector<int> PossiblePath(vector<int> )
+vector<vector<int>> ForPossiblePath(vector<vector<int>> path, vector<int> shortPath);
 
 int main(){
 vector<int> friendlyTroop = {0,3,9};
@@ -51,35 +51,76 @@ enemyTime = enemyPath*enemyTroop[2];
 cout<<"\nLe temps pris par les ennemis = "<<enemyTime<<endl;
 
 return(friendTime<enemyTime);
-}
 
+}
+vector<vector<int>> ForPossiblePath(vector<vector<int>> path, vector<int> shortPath){
+  vector<vector<int>> possiblePath;
+  vector<int> temp;
+  int sum = shortPath[0]+shortPath[1];
+  int Sum;
+  for(int i=0; i<6; i++){
+    Sum = path[i][0]+path[i][1];
+    if(sum<=-1 && Sum==-1){
+
+             for(int j=0; j<2; j++){
+                temp.push_back(path[i][j]);
+             }
+             possiblePath.push_back(temp);
+             temp.clear();
+    }
+    else if(sum==0 && Sum==0){
+            for(int k=0; k<2; k++){
+                temp.push_back(path[i][k]);
+            }
+            possiblePath.push_back(temp);
+            temp.clear();
+    }
+    else if(sum>=1 && Sum==1){
+            for(int l=0; l<2; l++){
+                temp.push_back(path[i][l]);
+            }
+            possiblePath.push_back(temp);
+            temp.clear();
+    }
+  }
+  return possiblePath;
+}
 int CountPath(vector<int> Test, vector<int> loggingCamp, vector<int> friendlyTroop, vector<vector<int>> Change, vector<int> Move, vector<vector<int>> impassableCells){
-int npath(0), verif(0), counter(0),stop(0);
+int npath(0), verif(0), counter(0),stop(0), size(0);
  vector<vector<int>> impassableMove;
  vector<int> local;
- vector<vector<int>> path = forChange{0,-1};
+ vector<vector<int>> path;
+ vector<vector<int>> possiblePath;
 
 do{
  impassableCells.push_back(Test);
  impassableMove = forMove(Test, impassableCells);
  local = Test;
- Test = fonction1(Test,loggingCamp);
+ Test = fonction1(Test,loggingCamp);  //genere le chemin le plus court
+ cout<<"Test :"<<endl;
  for(int x=0; x<2; x++){
     cout<<Test[x]<<endl;
  }
  cout<<"\n"<<endl;
  Move = fonction3(Test,local);
+ cout<<"Move :"<<endl;               //mouvement a faire pour se deplacer sur le cells le plus proche du but
  for(int m=0; m<2; m++){
     cout<<Move[m]<<endl;
  }
- cout<<"\n"<<endl;
-Change = forChange(Move);
-for(int i=0; i<6; i++){
+ path = forChange({0,-1});
+ possiblePath = ForPossiblePath(path, Move);  //parfois le mvmt a faire est impossible alors cette fonction genere des chemins 
+                                              //possibles qui remplacent ce mvt
+ size = possiblePath.size();
+ cout<<"possiblePath :\n"<<endl;
+for(int i=0; i<size; i++){
     for(int j=0; j<2; j++){
-        cout<<Change[i][j]<<endl;
+        cout<<possiblePath[i][j]<<endl;
     }
     cout<<"\n"<<endl;
 }
+ cout<<"\n"<<endl;
+Change = forChange(Move);
+
 do{ 
     verif=0;
     counter=0;
@@ -149,7 +190,7 @@ vector<vector<int>> forMove(vector<int> Test, vector<vector<int>> impassableCell
         vector<int> temp;
         for (int j = 0; j < 2; j++) {
             temp.push_back(impassableCells[i][j] - Test[j]);
-            cout << temp[j] << endl;
+            //cout << temp[j] << endl;
         }
         // Ajouter le vecteur temporaire à impassableMove
         impassableMove.push_back(temp);
@@ -243,14 +284,14 @@ vector<vector<int>> forChange(vector<int> Move){
             counter++;
         }
         if((i%2)==1 && i!=5){
-            cout<<"On randomise a et b\t i = "<<i<<endl;
+            //cout<<"On randomise a et b\t i = "<<i<<endl;
             a=rand()%(2);
             b=(a+1)%2;
         }
         if((counter%2)==0){
           sign = -sign;
         }
-        cout<<"sign = "<<sign<<endl;
+        //cout<<"sign = "<<sign<<endl;
         for(int j=0; j<2; j++){
           if(i==0 || i==5){
             temp.push_back((Move[j]*sign));
